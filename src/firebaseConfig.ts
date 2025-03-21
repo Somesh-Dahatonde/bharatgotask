6acf1d40948e2c339dac660b0b66e557682edc1f
somesh <sddahatonde22@gmail.com>
2025-03-22 01:37:46 +0530

    Update Firebase configuration to use environment variables and add .env to .gitignore

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA4GWfA6t4ILxegzqvXnGz7c3L5VB7B7MI",
-  authDomain: "bharatgo-135a5.firebaseapp.com",
-  projectId: "bharatgo-135a5",
-  storageBucket: "bharatgo-135a5.firebasestorage.app",
-  messagingSenderId: "712877184736",
-  appId: "1:712877184736:web:2af4874b23848f356d6285",
-  measurementId: "G-F641CCVG7N",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
